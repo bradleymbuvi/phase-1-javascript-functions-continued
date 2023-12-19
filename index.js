62cdf26d3f8d8dc600646b319b03725e2154f901
bradleymbuvi <bradwent@gmail.com>
2023-12-19 16:29:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(where="roller-skate"){
+    return `This Saturday, I want to ${where}!`
+}
+function mondayWork(where="go to the office"){
+     return`This Monday, I will ${where}.`
+}
+
+function wrapAdjective(sign = "*" )
+{
+    return function(name){
+        return `You are ${sign}${name}${sign}!`
+    }
+}
